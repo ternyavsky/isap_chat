15d6521b92a69cbe6378682cd0be0a8f172d2d79
Danik <elik@example.com>
2023-10-18 22:09:10 +0300

    new java project

---
+++ src/test/java/com/isap/isap/IsapChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isap.isap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsapChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
