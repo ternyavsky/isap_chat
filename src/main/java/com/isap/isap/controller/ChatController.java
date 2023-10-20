6a2572fb10c04abdd37100d0f07a738326429f08
Danik <elik@example.com>
2023-10-20 15:48:48 +0300

    all basic server functions

--- src/main/java/com/isap/isap/controller/ChatController.java
+++ src/main/java/com/isap/isap/controller/ChatController.java
@@ -1,14 +1,41 @@
 package com.isap.isap.controller;
 
 
+
+import com.isap.isap.controller.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
 
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     //метод получает данные для чата, а потом отправляет сообщение в частную тему чата
+    @MessageMapping("/message") //общий чат
+    @SendTo("/chatroom/public")
+    private Message receivePublicMessage(@Payload Message message){
+        return message;
+    }
+
+    //метод для отправки конкретному пользователю сообщения
+    @MessageMapping("/private-message")
+    public Message receivePrivateMessage(@Payload Message message){
+        //автоматически принимает префиксы с configureMessageBroker
+        //принимаем три параметра имя получателя, URL, само сообщение
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message);
 
 
+
+        return message;
+    }
 
 }
