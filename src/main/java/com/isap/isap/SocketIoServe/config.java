4c176b4bab75603702bcb04b663505a33246d8e9
Danik <elik@example.com>
2023-10-25 18:00:34 +0300

    write field for config

--- src/main/java/com/isap/isap/SocketIoServe/config.java
+++ src/main/java/com/isap/isap/SocketIoServe/config.java
@@ -6,10 +6,60 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-//sockjsон используем, для того , что он позволяет использовать планы резервного копирования на случай, если клиент не сможет подключиться через WebSocket.
+//sockjsон используем, для того, что он позволяет использовать планы резервного копирования на случай, если клиент не сможет подключиться через WebSocket.
 @Configuration
 @EnableWebSocketMessageBroker
-public class config implements WebSocketMessageBrokerConfigurer {
+public class config implements WebSocketMessageBrokerConfigurer{
+
+
+//данные отправляются без буферезации
+  private boolean tcpNoDelay = true;
+  //системный размер буфера
+  private int tcpSendBufferSize=-1;
+  //системный размер буфера для передачи данных через сокеты
+  private int tcpReceiveBufferSize=-1;
+  // сокет будет отправлять "ping-пойнты" через определенные промежутки времени,
+  // чтобы убедиться, что другой конец все еще работает.
+  private boolean tcpKeepAlive=true;
+  // управляет временем, которое сокет будет ждать ответа перед закрытием соединения
+  private int soLinger = -1;
+  // определяет максимальное количество одновременных соединений, которые сервер может обрабатывать
+  private int acceptBackLog = -1;
+  //определяет минимальный размер буфера, который используется для записи данных на стороне сервера
+  private int writeBufferWaterWarkLow = -1;
+  //определяет максимальный размер буфера, который используется для записи данных на стороне сервера
+  private int writeBufferWaterWarkHigh = -1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   @Override
 
     public void configureMessageBroker(MessageBrokerRegistry registry){
